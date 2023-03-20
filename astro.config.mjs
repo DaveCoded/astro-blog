c8e3e4d8a220ec334d7fbc534af25d9f9928d1f0
Dave Bernhard <davidbernhard@live.com>
2023-03-20 19:34:57 +0000

    style code blocks

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,19 @@ import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [mdx()]
+    integrations: [mdx()],
+
+    markdown: {
+        shikiConfig: {
+            // Choose from Shiki's built-in themes (or add your own)
+            // https://github.com/shikijs/shiki/blob/main/docs/themes.md
+            theme: 'nord'
+            // Add custom languages
+            // Note: Shiki has countless langs built-in, including .astro!
+            // https://github.com/shikijs/shiki/blob/main/docs/languages.md
+            //   langs: [],
+            // Enable word wrap to prevent horizontal scrolling
+            // wrap: true
+        }
+    }
 });
